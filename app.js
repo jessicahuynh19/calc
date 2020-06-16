4fba34d172558f7c7c8ea1b58ba9f5a83d0903b4
Jessica Huynh <huynhjessica19@gmail.com>
2020-06-15 22:00:25 -0700

    adding selectors

--- app.js
+++ app.js
@@ -2,17 +2,33 @@ const game = ()=> {
     let  pScore = 0; //two variables that are needed for global scope
     let cScore = 0;
 
+    //Start the game
     const startGame = ()=> {
-        const playBtn = document.querySelector('.intro button');
-        const introScreen = document.querySelector('.intro');
-        const match = document.querySelector('.match');
+        const playBtn = document.querySelector(".intro button");
+        const introScreen = document.querySelector(".intro");
+        const match = document.querySelector(".match");
 
-        playBtn.addEventListener('click', () =>{
+        playBtn.addEventListener("click", () =>{
             introScreen.classList.add("fadeOut");
+            match.classList.add("fadeIn");
         });
     };
+    //Play Match
+    const playMatch = () => {
+        const options = document.querySelectorAll(".options button");
+        const playerHand = document.querySelector(".player-hand");
+        const computerHand = document.querySelector(".computer-hand");
+        //computer options
+        const computerOptions = ['rock', 'paper', 'scissors'];
+
+        const computerNumber = Math.floor(Math.random() * 3);
+        
+        const computerNumber = Math.floor(Math.random() * 3);
+    };
+
     //Call all inner function
     startGame();
+    playMatch();
    
 };
 
